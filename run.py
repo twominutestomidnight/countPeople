348f7472cb41d2b70707b44fa76929e76b4600bc
twominutestomidnight <45543658+twominutestomidnight@users.noreply.github.com>
2019-02-22 10:56:22 +0200

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-from handleAlarm import alarm
-from getCurrentPeople import getStartPeopleValue
-
-
-if __name__ == '__main__':
-    startValue = getStartPeopleValue()
-    print(startValue)
-    alarm(startValue)
